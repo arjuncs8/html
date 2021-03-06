16ee29e7da8f9e93cf76fc544c4755053d6676ea
arjuncs8 <50917643+arjuncs8@users.noreply.github.com>
2021-03-06 16:39:56 +0530

    Add files via upload

---
+++ src/app/angcontent/angcontent.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-angcontent',
+  templateUrl: './angcontent.component.html',
+  styleUrls: ['./angcontent.component.css']
+})
+export class AngcontentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/anggal/anggal.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-anggal',
+  templateUrl: './anggal.component.html',
+  styleUrls: ['./anggal.component.css']
+})
+export class AnggalComponent  {
+
+}
